7e8524b5c23ece057c913f0bf7372d9bec567df0
Mateusz Klos <novopl@gmail.com>
2019-01-15 11:50:48 +0000

    Change license to Apache 2.0

--- src/serafin/__init__.py
+++ src/serafin/__init__.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
+# Copyright 2017-2019 Mateusz Klos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 """
-
 **serafin** is a serialization system that allows flexible serialization
 of any type of object according to a provided field spec. The field spec tells
 the serialize which attribute/fields/members of the given object should be
